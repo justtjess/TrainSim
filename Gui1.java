24f34646a7be0dee60ad1918e5331f1bdc3dc443
Jazarie Thach <jazariethach@umail.ucsb.edu>
2015-01-15 22:10:08 -0800

    exit on close

--- Gui1.java
+++ Gui1.java
@@ -2,11 +2,12 @@ import javax.swing;
 
 public class Gui1{
 	public static void main (String[] args){
-		Jframe frame = new Jframe();
+		JFrame frame = new JFrame();
 		JButton start = new JButton("START");
 
 		frame.setSize(400,400);
 		frame.setVisible(true);
 		frame.getContentPane().add(start);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
