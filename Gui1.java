581f45b4b41278fe0e5dfa5fee0ac1f53b260cfe
Felicia <xftruong@hotmail.com>
2015-01-16 09:54:39 -0800

    Switched screen/bg

--- Gui1.java
+++ Gui1.java
@@ -3,28 +3,32 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Gui1 {
-public static void main (String[] args){
-	JFrame frame = new JFrame();
-	ImagePanel panel = new ImagePanel("train.jpg");
-	panel.setPreferredSize(new Dimension(800, 600));
-	JButton start = new JButton("START");
-	panel.add(start);
-	frame.getContentPane().add(panel, BorderLayout.CENTER);
-	frame.setSize(800,600);
+	public static void main (String[] args){
+		JFrame frame = new JFrame();
+		ImagePanel panel = new ImagePanel("train.jpg");
+		panel.setPreferredSize(new Dimension(800, 600));
+		JButton start = new JButton("START");
+		panel.add(start);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.setSize(800,600);
 	
-	// frame.getContentPane().add(start);
-	start.setFont(new Font( "Arial", Font.BOLD, 18));
-	start.setVisible(true);
-	start.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae) {
-				// frame.setVisible(false);
-// 				new SecondFrame();
-			start.setVisible(false);
-			}
-	});
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setVisible(true);
-}
+		// frame.getContentPane().add(start);
+		start.setFont(new Font( "Arial", Font.BOLD, 18));
+		start.setVisible(true);
+		start.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ae) {
+					// frame.setVisible(false);
+	// 				new SecondFrame();
+				 start.setVisible(false);
+			// 	start.dispose();
+				ImagePanel panel2 = new ImagePanel("bg.jpg");
+				panel2.setPreferredSize(new Dimension(800, 600));
+				frame.getContentPane().add(panel2, BorderLayout.CENTER);
+				}
+		});
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
 
 	
