cc4fc2bf1ad9385a25d333e4a708705225e6ae69
Josephine Vo <jovo123@gmail.com>
2015-01-15 22:52:14 -0800

    testing git shit again

--- Gui1.java
+++ Gui1.java
@@ -1,12 +1,14 @@
-import javax.swing;
+import javax.swing.*;
 
 public class Gui1{
 	public static void main (String[] args){
-		Jframe frame = new Jframe();
+		JFrame frame = new JFrame();
 		JButton start = new JButton("START");
 
 		frame.setSize(400,400);
 		frame.setVisible(true);
 		frame.getContentPane().add(start);
+		start.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
