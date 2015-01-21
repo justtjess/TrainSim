12aed68cd82dbb64986aeccaac059a15abe156ac
justtjess <jessicahuang@umail.ucsb.edu>
2015-01-21 14:14:21 -0800

    new animal and images

--- Gui1.java
+++ Gui1.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Graphics;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
 
 public class Gui1 {
 	public static void main (String[] args){
@@ -8,6 +11,11 @@ public class Gui1 {
 		ImagePanel panel = new ImagePanel("train.jpg");
 		panel.setPreferredSize(new Dimension(800, 600));
 		JButton start = new JButton("START");
+
+		Graphics g;
+		BufferedImage image;
+		Animals animals;
+
 		panel.add(start);
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		frame.setSize(800,600);
@@ -17,14 +25,19 @@ public class Gui1 {
 		start.setVisible(true);
 		start.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent ae) {
-					// frame.setVisible(false);
-	// 				new SecondFrame();
-				 start.setVisible(false);
-			// 	start.dispose();
-				frame.remove(panel);
-				ImagePanel panel2 = new ImagePanel("bg.jpg");
-				panel2.setPreferredSize(new Dimension(800, 600));
-				frame.getContentPane().add(panel2, BorderLayout.CENTER);
+					//frame.setVisible(false);
+	 				//new SecondFrame();
+					start.setVisible(false);
+				 	//start.dispose();
+					frame.remove(panel);
+					ImagePanel panel2 = new ImagePanel("bg.jpg");
+					panel2.setPreferredSize(new Dimension(800, 600));
+					frame.getContentPane().add(panel2, BorderLayout.CENTER);
+
+					image = Image.load("test_animals(birds).png");
+					animals = new Animals(image);
+					g.drawImage(animals.image(0,0,32,32),5,5,null);
+
 				}
 		});
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
